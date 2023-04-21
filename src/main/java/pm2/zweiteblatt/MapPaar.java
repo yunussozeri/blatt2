293919de7ca7b6350849043ac736423ddf507e26
yunussozeri <yunus.sozeri.254@gmail.com>
2023-04-21 14:41:44 +0200

    Added documentation and constructors

--- src/main/java/pm2/zweiteblatt/MapPaar.java
+++ src/main/java/pm2/zweiteblatt/MapPaar.java
@@ -11,6 +11,15 @@ public class MapPaar <K,V> implements Map.Entry<K, V>{
 
     private K key;
     private V value;
+    
+    public MapPaar(){
+        
+    }
+    
+    public MapPaar(K key, V value){
+        this.key=key;
+        this.value = value;
+    }
     @Override
     public K getKey() {
         return key;
--- src/main/java/pm2/zweiteblatt/PM2Map.java
+++ src/main/java/pm2/zweiteblatt/PM2Map.java
@@ -11,33 +11,61 @@ import java.util.Set;
 public class PM2Map<K, V> implements Map<K, V> {
 
     private MapPaar<K, V>[] pairs;
-    private int length;
+    private int size;
     private int cardinality;
     private MapPaar last;
 
+    /**
+     * Constructor for PM2Map Object.
+     */
     public PM2Map() {
-
-    }
-
-    public PM2Map(MapPaar<K, V>[] pairs) {
-        this.pairs = pairs;
-        length = 0;
+        size = 0;
         cardinality = 0;
     }
 
+    /**
+     * PM2Map Constructor with generic MapPaar array parameter.
+     * 
+     * @param pairs 
+     */
+    public PM2Map(MapPaar<K, V>[] pairs) {
+        this.pairs = pairs;
+        size = 0;
+        cardinality = 0;
+    }
+
+    /**
+     * Returns the cardinality of the Map.
+     * This gives information on how many elements are contained in the PM2Map.
+     * 
+     * @return cardinality, the number of elements
+     */
     @Override
     public int size() {
         return cardinality;
     }
 
+    /**
+     * Tells if the PM2Map is empty.
+     * 
+     * @return true, if there is no elements contained.
+     *         false, if there are some elements contained.
+     */
     @Override
     public boolean isEmpty() {
         return cardinality == 0;
     }
 
+    /**
+     * Checks if the given key exists in the PM2Map.
+     * 
+     * @param key the key that is searched for.
+     * @return true, if the key already exists.
+     *         false, if the key does not exists.
+     */
     @Override
     public boolean containsKey(Object key) {
-        for (MapPaar pair : pairs) {
+        for (var pair : pairs) {
             if (pair != null) {
                 if (pair.getKey().equals(key)) {
                     return true;
@@ -47,9 +75,16 @@ public class PM2Map<K, V> implements Map<K, V> {
         return false;
     }
 
+    /**
+     * Checks if the given value exists in the PM2Map.
+     * 
+     * @param value the key that is searched for.
+     * @return true, if the value already exists.
+     *         false, if the value does not exist.
+     */
     @Override
     public boolean containsValue(Object value) {
-        for (MapPaar pair : pairs) {
+        for (var pair : pairs) {
             if (pair != null) {
                 if (pair.getValue().equals(value)) {
                     return true;
@@ -58,11 +93,18 @@ public class PM2Map<K, V> implements Map<K, V> {
         }
         return false;
     }
-
+    
+    /**
+     * Returns the value for a given key in PM2Map.
+     * 
+     * @param key the key that is searched for.
+     * @return val the value stored by the key.
+     *         null, if the key does not exist.
+     */
     @Override
     public V get(Object key) {
         V val = null;
-        for (MapPaar pair : pairs) {
+        for (var pair : pairs) {
             if (pair != null) {
                 if (pair.getKey().equals(key)) {
                     val = (V) pair.getValue();
@@ -72,16 +114,65 @@ public class PM2Map<K, V> implements Map<K, V> {
         return val;
     }
 
+    /**
+     * Puts a key-value pair into the Map.
+     * If the key already exists, then the value is replaced 
+     * and the old value is returned.
+     * If the key does not exist, 
+     * a new key-value pair is appended to the list,
+     * and null is returned.
+     * 
+     * @param key the key that shall be added
+     * @param value the value that shall be added.
+     * @return oldValue, if the key already exists.
+     *         null, if a new element is appended.
+     */
     @Override
     public V put(K key, V value) {
+
+        // if the key already exists, 
+        if (containsKey(key)) {
+            //replace the value and 
+            for (var pair : pairs) {
+                if (pair != null) {
+                    if (pair.getKey().equals(key)) {
+                        var oldValue = pair.getValue();
+                        pair.setValue(value);
+                        // return the old value
+                        return oldValue;
+                    }
+                }
+            }
+        }
+
         // check size
-        // increase size if needed
-        
+        if (cardinality == size) {
+            // increase size if needed
+            increaseSize();
+        }
+
+        MapPaar<K, V> pair = new MapPaar<K, V>(key, value);
         // loop until last element
         // add new element to the next index
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        return null;
     }
 
+    /**
+     * Doubles the size of the generic MapPaar array.
+     */
+    private void increaseSize() {
+        MapPaar<K, V>[] largerArray = new MapPaar[size * 2];
+        System.arraycopy(pairs, 0, largerArray, 0, size);
+        pairs = largerArray;
+    }
+
+    /**
+     * Removes the key-value pair with the given key from the PM2Map.
+     * 
+     * @param key is the key that is searched.
+     * @return value is the value of the pair that is removed.
+     */
     @Override
     public V remove(Object key) {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -92,10 +183,13 @@ public class PM2Map<K, V> implements Map<K, V> {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * Clears the Map by removing elements in the MapPaar array.
+     */
     @Override
     public void clear() {
         for (int i = 0; i < cardinality; i++) {
-            pairs[i] = new MapPaar<K, V>();
+            pairs[i] = null;
         }
     }
 
